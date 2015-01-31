905417983a2eadb616c88e699eb8b9a72c7e200c
Orvar Segerström <orvarsegerstrom@gmail.com>
2015-01-31 15:58:23 +0100

    Initial commit

---
+++ Common.lua
@@ -0,0 +1,186 @@
+local addon, ns = ...
+local _G = _G
+
+local unpack, pairs, ipairs, table, math = unpack, pairs, ipairs, table, math
+local next = next
+local IsSpellKnown = IsSpellKnown
+local C_Garrison = C_Garrison
+local GetItemInfo = GetItemInfo
+local GetCoinTextureString = GetCoinTextureString
+local UIParent = UIParent
+local ITEM_SOULBOUND = ITEM_SOULBOUND
+
+ns.C = LibStub('LibColors-1.0').color
+
+ns.GetShortCoinString = function(value)
+  if value > 100*100 then
+    return GetCoinTextureString(math.floor(value/100/100)*100*100)
+  elseif value > 100 then
+    return GetCoinTextureString(math.floor(value/100)*100)
+  else
+    return GetCoinTextureString(value)
+  end
+end
+
+ns.GetDiffCoinString = function(value)
+  local base = ns.GetShortCoinString(math.abs(value))
+  if value < 0 then
+    return ns.C("red", "-"..base)
+  elseif value > 0 then
+    return ns.C("green", "+"..base)
+  else
+    return "±0"
+  end
+end
+
+ns.GetItemValue = function(id, isSoulbound, de_capability)
+  local equivItems = ns.EquivItems(id)
+  if equivItems then
+    local sum = 0
+    for _,item in ipairs(equivItems) do
+      sum = sum + ns.GetItemValue(item.id)*item.count
+    end
+    return sum, "Invested"
+  end
+
+  local _, link, _, _, _, _, _,
+        _, _, _, vendor_price = GetItemInfo(id)
+  local value = vendor_price or 0
+
+  if isSoulbound and ns.CanDisenchant(de_capability) then
+    local disenchantValue = ns.GetDisenchantValue(link, de_capability)
+    if disenchantValue > value then
+      return disenchantValue, "Disenchant"
+    elseif disenchantValue ~= 0 then
+    end
+  elseif _G.AucAdvanced then
+    local market, seen = _G.AucAdvanced.API.GetMarketValue(link)
+    if market and seen > 0 then
+      return market, "AH"
+    end
+  end
+
+  return value, "Vendor"
+end
+
+-- For soulbound items
+local price_equivalence = {
+  -- 50x Primal Sprit -> 1x Savage Blood
+  [120945] = {{ id = 118472, count = 1.0/50.0 }},
+  -- 10x Truesteel Ingot <- 20x True Iron Ore, 10x Blackrock Ore
+  [108257] = {{ id = 109119, count = 2 }, { id = 109118, count = 1 }},
+  -- 10x Taladite Crystal <- 10x True Iron Ore, 20x Blackrock Ore
+  [170832] = {{ id = 109119, count = 1 }, { id = 109118, count = 2 }},
+  -- 10x War Paints <- 10x Cerulean Pigment
+  [112377] = {{ id = 114931, count = 1 }},
+  -- 10x Hexweave Cloth <- 20x Sumtuous Fur, 10x Gorgond Flytrap
+  [170832] = {{ id = 111557, count = 2 }, { id = 109126, count = 1 }},
+  -- 10x Gearspring Parts <- 10x True Iron Ore, 10x Blackrock Ore
+  [111366] = {{ id = 109119, count = 1 }, { id = 109118, count = 1 }},
+  -- 10x Burnished Leather <- 20x Raw Beast Hide, 10x Gorgond Flytrap
+  [110611] = {{ id = 110609, count = 2}, { id = 109126, count = 1}}
+}
+ns.EquivItems = function(id)
+  return price_equivalence[id]
+end
+
+
+ns.GetDisenchantValue = function(link, capability)
+  if not _G.Enchantrix then
+    return 0
+  end
+  local data = _G.Enchantrix.Storage.GetItemDisenchants(link)
+  if not data then
+    return 0
+  end
+
+  local lines
+
+  local total = data.total
+  local totalFive = 0
+  local totalHSP, totalMed, totalMkt, totalFive = 0,0,0,0
+  local totalNumber, totalQuantity
+  local allFixed = true
+
+  if (total and total[1] > 0) then
+    totalNumber, totalQuantity = unpack(total)
+    for result, resData in pairs(data) do
+      if (result ~= "total") then
+        if (not lines) then lines = {} end
+
+        local resNumber, resQuantity = unpack(resData)
+        local style, extra = _G.Enchantrix.Util.GetPricingModel()
+        local hsp, med, mkt, five, fix = _G.Enchantrix.Util.GetReagentPrice(result, extra)
+        local resProb, resCount = resNumber/totalNumber, resQuantity/resNumber
+        local resYield = resProb * resCount;  -- == resQuantity / totalNumber;
+        local resHSP, resMed, resMkt, resFive, resFix = (hsp or 0)*resYield, (med or 0)*resYield, (mkt or 0)*resYield, (five or 0)*resYield, (fix or 0)*resYield
+        if (fix) then
+          resHSP, resMed, resMkt, resFive = resFix,resFix,resFix,resFix
+        else
+          allFixed = false
+        end
+        totalHSP = totalHSP + resHSP
+        totalMed = totalMed + resMed
+        totalMkt = totalMkt + resMkt
+        totalFive = totalFive + resFive
+      end
+    end
+  end
+  return totalFive * ns.GetDisenchantFactor(capability)
+end
+
+ns.GetDisenchantFactor = function(capability)
+  if capability == "FULL" then
+    return 1
+  elseif capability == "GARRISON" then
+    return 0.3 -- Seems to be about right for DEing epics at level 3
+  end
+  return 0
+end
+
+local DISENCHANTING = 13262
+ns.GetDECapability = function()
+  if IsSpellKnown(DISENCHANTING) then
+    return "FULL"
+  end
+  for _, building in next, C_Garrison.GetBuildings() do
+    local buildingID = building.buildingID
+    if buildingID == 93 or buildingID == 125 or buildingID == 126 then
+      return "GARRISON"
+    end
+  end
+  return "NONE"
+end
+
+ns.CanDisenchant = function(capability)
+  return capability == "FULL" or capability == "GARRISON"
+end
+
+
+
+
+local ItemTooltip = CreateFrame("GameTooltip", "temptt")
+do
+  local fontstrings = {}
+  for i = 1, 6 do
+    local L,R = ItemTooltip:CreateFontString(), ItemTooltip:CreateFontString()
+    L:SetFontObject(GameFontNormal)
+    R:SetFontObject(GameFontNormal)
+    ItemTooltip:AddFontStrings(L,R)
+    table.insert(fontstrings,L)
+  end
+  ItemTooltip.strings = fontstrings
+  --ItemTooltip:SetOwner(UIParent,"ANCHOR_NONE")
+  --ItemTooltip:SetHyperlink('item:')
+end
+ns.IsSlotSoulbound = function(bag, slot)
+  ItemTooltip:SetOwner(UIParent,"ANCHOR_NONE")
+  ItemTooltip:SetBagItem(bag, slot)
+
+  for i,v in ipairs(ItemTooltip.strings) do
+    if v:GetText() == ITEM_SOULBOUND then
+      return true
+    end
+  end
+  return false
+end
---
+++ Main.lua
@@ -0,0 +1,254 @@
+local addon, ns = ...
+local unpack, pairs, ipairs, table, math = unpack, pairs, ipairs, table, math
+local _G = _G
+
+local GetMoney = GetMoney
+local GetItemInfo = GetItemInfo
+local GetContainerItemInfo = GetContainerItemInfo
+local GetContainerNumSlots = GetContainerNumSlots
+local GetContainerItemID = GetContainerItemID
+local NUM_BAG_SLOTS, NUM_BANKBAGSLOTS = NUM_BAG_SLOTS, NUM_BANKBAGSLOTS
+local GetNumEquipmentSets = GetNumEquipmentSets
+local GetEquipmentSetInfo = GetEquipmentSetInfo
+local GetEquipmentSetLocations = GetEquipmentSetLocations
+local EquipmentManager_UnpackLocation = EquipmentManager_UnpackLocation
+
+local C = ns.C
+
+local PLAYER = UnitName("player")
+local FACTION = UnitFactionGroup("player")
+local CLASS = UnitClass("player")
+local REALM = GetRealmName()
+local DB = {}
+local SessionStart = {}
+
+local function CalculateTotalMoney()
+  if not DB or not DB[FACTION] or not DB[FACTION][REALM] then
+    return 0
+  end
+  local total = 0
+  for _,player in pairs(DB[FACTION][REALM]) do
+    total = total + player.money
+  end
+  return total
+end
+
+local function CalculateTotalItemValue()
+  if not DB or not DB[FACTION] or not DB[FACTION][REALM] then
+    return 0
+  end
+  local total = 0
+  for _,player in pairs(DB[FACTION][REALM]) do
+    for _,item in ipairs(player.bagItems) do
+      local unit_worth, _ =
+        ns.GetItemValue(item.id, item.soulbound, player.disenchant_capability)
+      total = total + unit_worth * item.count
+    end
+    for _,item in ipairs(player.bankItems) do
+      local unit_worth, _ =
+        ns.GetItemValue(item.id, item.soulbound, player.disenchant_capability)
+      total = total + unit_worth * item.count
+    end
+  end
+  return total
+end
+
+local function CalculateWorth()
+  return CalculateTotalMoney() + CalculateTotalItemValue()
+end
+
+
+
+
+local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
+local icon = {
+  iconfile="Interface\\icons\\achievement_challengemode_gold",
+  coords={0.1,0.9,0.1,0.9}
+}
+local dataobj = ldb:NewDataObject("Water", {
+  type          = "data source",
+  text          = "",
+  icon          = icon.iconfile, -- default or custom icon
+  staticIcon    = icon.iconfile, -- default icon only
+  iconCoords    = icon.coords,
+})
+
+local LibQTip = LibStub('LibQTip-1.0')
+function dataobj:OnEnter()
+  local tooltip = LibQTip:Acquire(addon.."TT", 3, "LEFT", "RIGHT", "RIGHT")
+  self.tooltip = tooltip
+  buildTooltip(tooltip)
+  tooltip:SmartAnchorTo(self)
+  tooltip:Show()
+end
+function dataobj:OnLeave()
+  -- Release the tooltip
+  LibQTip:Release(self.tooltip)
+  self.tooltip = nil
+end
+function buildTooltip(tooltip)
+  local totalItemValue = CalculateTotalItemValue()
+  local totalMoney = CalculateTotalMoney()
+  local items = {}
+  local top = {}
+  local byCharacter = {}
+  local byProspect = {}
+
+  -- Aggregate items, sum by character, sum by value prospect
+  for name,player in pairs(DB[FACTION][REALM]) do
+    byCharacter[name] = {
+      money = player.money,
+      itemValue = 0,
+      class = player.class
+    }
+    local function add(item)
+      local unit_worth, prospect = ns.GetItemValue(item.id, item.soulbound, player.disenchant_capability)
+      local x = unit_worth * item.count
+      byCharacter[name].itemValue = byCharacter[name].itemValue + x
+      byProspect[prospect] = (byProspect[prospect] or 0) + x
+
+      items[item.id] = items[item.id] or {
+        unit_worth = unit_worth,
+        count = 0,
+        prospect = prospect
+      }
+      items[item.id].count = items[item.id].count + item.count
+    end
+    for _,item in ipairs(player.bagItems) do
+      add(item)
+    end
+    for _,item in ipairs(player.bankItems) do
+      add(item)
+    end
+  end
+
+  -- Build item list sorted by value
+  for id,item in pairs(items) do
+    local val = item.count * item.unit_worth
+    local _, link, _ = GetItemInfo(id)
+    table.insert(top, {link = link, count = item.count, value = val})
+  end
+  local function compareByValue(a,b)
+    return a.value > b.value
+  end
+  table.sort(top, compareByValue)
+
+  -- Build tooltip
+  tooltip:AddHeader(
+    C('dkyellow', 'Per character'),
+    C('dkyellow', 'Gold'),
+    C('dkyellow', 'Items'))
+  tooltip:AddSeparator()
+  for name,p in pairs(byCharacter) do
+    tooltip:AddLine(C(p.class, name),
+      ns.GetShortCoinString(p.money),
+      ns.GetShortCoinString(p.itemValue))
+  end
+  tooltip:AddSeparator()
+  tooltip:AddLine("Total",
+    ns.GetShortCoinString(totalMoney),
+    ns.GetShortCoinString(totalItemValue))
+  tooltip:AddLine("Session",
+    ns.GetDiffCoinString(totalMoney-SessionStart.totalMoney),
+    ns.GetDiffCoinString(totalItemValue-SessionStart.totalItemValue))
+  tooltip:AddLine()
+
+  tooltip:AddHeader(
+    C('dkyellow', 'Breakdown'),
+    nil,
+    C('dkyellow', 'Value'))
+  tooltip:AddSeparator()
+  for p,value in pairs(byProspect) do
+    tooltip:AddLine(p, nil, ns.GetShortCoinString(value))
+  end
+  tooltip:AddLine()
+
+  tooltip:AddHeader(
+    C('dkyellow', 'Top items'),
+    C('dkyellow', 'Count'),
+    C('dkyellow', 'Value'))
+  tooltip:AddSeparator()
+  for i,item in ipairs(top) do
+    tooltip:AddLine(item.link, item.count, ns.GetShortCoinString(item.value))
+    if i > 20 then
+      return
+    end
+  end
+end
+
+local updateFrame = CreateFrame("Frame")
+local function refresh(self, event, ...)
+  if event == "PLAYER_ENTERING_WORLD" then
+    updateFrame:RegisterEvent("BAG_UPDATE")
+    updateFrame:RegisterEvent("UNIT_INVENTORY_CHANGED")
+    updateFrame:RegisterEvent("PLAYER_MONEY")
+    updateFrame:RegisterEvent("BANKFRAME_OPENED")
+  end
+  local money = GetMoney()
+  local bagItems = {}
+  local bankItems = {}
+
+  local blacklist = {}
+  for i = 1, GetNumEquipmentSets() do
+    local name = GetEquipmentSetInfo(i);
+    local infoArray = GetEquipmentSetLocations(name);
+    for _,value in pairs(infoArray) do
+      if value > 1 then
+        local player, bank, bags, voidStorage, slot, bag = EquipmentManager_UnpackLocation(value)
+        if bags then
+          blacklist[bag] = blacklist[bag] or {}
+          blacklist[bag][slot] = true
+        end
+      end
+    end
+  end
+
+  for bagID=-1,NUM_BAG_SLOTS+NUM_BANKBAGSLOTS do
+    local targetTable = bagItems
+    if bagID < 0 or bagID > NUM_BAG_SLOTS then
+      targetTable = bankItems
+    end
+    for slot = 1,GetContainerNumSlots(bagID) do
+      local _, itemCount, _, _, _, _, _ = GetContainerItemInfo(bagID, slot)
+      if itemCount ~= nil and itemCount > 0 and (blacklist[bagID] == nil or blacklist[bagID][slot] == nil) then
+        local itemId = GetContainerItemID(bagID, slot)
+        local soulbound = ns.IsSlotSoulbound(bagID, slot)
+        table.insert(targetTable, {id = itemId, count = itemCount, soulbound = soulbound})
+      end
+    end
+  end
+
+  -- AceDB would be great, eh?
+  DB[FACTION] = DB[FACTION] or {}
+  DB[FACTION][REALM] = DB[FACTION][REALM] or {}
+  DB[FACTION][REALM][PLAYER] = DB[FACTION][REALM][PLAYER] or {}
+
+  DB[FACTION][REALM][PLAYER].class = CLASS
+  DB[FACTION][REALM][PLAYER].disenchant_capability = ns.GetDECapability()
+  DB[FACTION][REALM][PLAYER].money = money
+  DB[FACTION][REALM][PLAYER].bagItems = bagItems
+  if #bankItems > 0 or not DB[FACTION][REALM][PLAYER].bankItems then
+    DB[FACTION][REALM][PLAYER].bankItems = bankItems
+  end
+
+  -- Record worth at login - not reliable on first login (why?)
+  if SessionStart.totalMoney == nil then
+    SessionStart = {
+      totalMoney = CalculateTotalMoney(),
+      totalItemValue = CalculateTotalItemValue()
+    }
+  end
+  dataobj.text = ns.GetShortCoinString(CalculateWorth())
+end
+
+local initFrame = CreateFrame("Frame")
+initFrame:RegisterEvent("ADDON_LOADED")
+initFrame:SetScript("OnEvent", function(self, event, arg1)
+  if event == "ADDON_LOADED" and arg1 == addon then
+    _G[addon.."DB"] = _G[addon.."DB"] or {}
+    DB = _G[addon.."DB"]
+    updateFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+    updateFrame:SetScript("OnEvent", refresh)
+    LibStub('LibDBIcon-1.0'):Register(addon, dataobj, DB.dbicon)
+  end
+end)
